cb27773e747b143b5aedab7456556616fc7683ed
gezhigang <zhigang.ge@renlijia.com>
2026-02-24 10:11:24 +0800

    Update Inkess to v2.3.1 with correct SHA256

--- Casks/inkess.rb
+++ Casks/inkess.rb
@@ -1,6 +1,6 @@
 cask "inkess" do
   version "2.3.1"
-  sha256 "PLACEHOLDER"
+  sha256 "55cfd07a506d759929d5609e8609a09b6e22fa70f429b0f0567d228fbb840a7f"
 
   url "https://github.com/gezhigang000/inkess-app/releases/download/v#{version}/Inkess_2.2.0_aarch64.dmg"
   name "Inkess"
